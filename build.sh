0ef3919feacc591c45512b60301848faa6adfbf9
Tom Hennen <tom.hennen@gmail.com>
2024-12-17 10:15:17 -0500

    add build_container
    Signed-off-by: Tom Hennen <tom.hennen@gmail.com>

--- build.sh
+++ build.sh
@@ -4,11 +4,13 @@ set -e
 # Pass tool names to build as an argument.
 # e.g. build.sh foo bar
 
+# TODO: Have this use build_container...
+
 for tool in $@
 do
     echo "Build $tool..."
     pushd tools/$tool
-    docker buildx build -f Dockerfile -o type=docker -t tool_$tool .
+    docker buildx build --sbom=true -f Dockerfile -o type=docker -t tool_$tool .
     popd
     echo "$tool done"
 done
--- run.sh
+++ run.sh
@@ -8,9 +8,12 @@ for tool in $@;
 do
     echo "Running $tool..."
     mkdir -p ./metadata/$tool
+    mkdir -p ./dist/$tool
     docker run \
+       --mount type=bind,source=./dist/$tool,target=/dist \
 	   --mount type=bind,source=./metadata/$tool,target=/metadata \
 	   --mount type=bind,source=./src,target=/src,readonly \
+       -v /var/run/docker.sock:/var/run/docker.sock \
 	   tool_$tool
     echo "$tool done"
 done
