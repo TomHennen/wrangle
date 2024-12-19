130c7c886f0a78654e21305cddcd0cd2883c8edb
Tom Hennen <TomHennen@users.noreply.github.com>
2024-12-19 15:42:41 +0000

    Get scan output in both sarif and human readable formats

--- tools/osv/run.sh
+++ tools/osv/run.sh
@@ -5,3 +5,5 @@ echo "osv"
 # Run a scan over all of source and ignore errors, because OSV likes to fail
 # too often. :) (Maybe it generates an error code when it finds problems?)
 /osv-scanner --format sarif --output /metadata/osv.sarif -r /src || true
+# Run it again for the markdown. (if only we could output in multiple formats from one run...)
+/osv-scanner --format markdown --output /metadata/osv.md -r /src || true
