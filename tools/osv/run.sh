a530d281dd9eb249859752c0002c715f1c04b802
Tom Hennen <TomHennen@users.noreply.github.com>
2024-12-19 15:15:24 +0000

    Use the right name for sarif and try to upload.

--- tools/osv/run.sh
+++ tools/osv/run.sh
@@ -4,4 +4,4 @@ set -e
 echo "osv"
 # Run a scan over all of source and ignore errors, because OSV likes to fail
 # too often. :) (Maybe it generates an error code when it finds problems?)
-/osv-scanner --format sarif --output /metadata/osv.json -r /src || true
+/osv-scanner --format sarif --output /metadata/osv.sarif -r /src || true
--- tools/zizmor/run.sh
+++ tools/zizmor/run.sh
@@ -2,4 +2,4 @@
 set -e
 
 echo "zizmor"
-/usr/local/cargo/bin/zizmor --format sarif -o `find /src/ -name "*.yml"` > /metadata/zizmor.json
+/usr/local/cargo/bin/zizmor --format sarif -o `find /src/ -name "*.yml"` > /metadata/zizmor.sarif
