0c0338bb1db85305d730cbe2ce063a17b92eb30b
Tom Hennen <TomHennen@users.noreply.github.com>
2024-12-20 10:51:24 -0500

    move details lower...

--- run.sh
+++ run.sh
@@ -23,7 +23,16 @@ do
        -v /var/run/docker.sock:/var/run/docker.sock \
 	   ghcr.io/tomhennen/wrangle/$tool:main | tee ./metadata/$tool/output.txt || WRANGLE_EXIT_STATUS=1; TOOL_STATUS="Failed"
     echo "$tool $TOOL_STATUS"
-    echo "| $tool | $TOOL_STATUS | <pre><code>`cat ./metadata/$tool/output.txt`</code></pre> |" >> $SUMMARY_FILE
+    echo "| $tool | $TOOL_STATUS | [Details](#$tool-details) |" >> $SUMMARY_FILE
+done
+
+# Add in the details
+for tool in $@;
+do
+    echo "##$tool Details" >> $SUMMARY_FILE
+    echo "```" >> $SUMMARY_FILE
+    cat ./metadata/$tool/output.txt >> $SUMMARY_FILE
+    echo "```" >> $SUMMARY_FILE
 done
 echo "Done with all tools. Exiting with $WRANGLE_EXIT_STATUS"
 exit $WRANGLE_EXIT_STATUS
