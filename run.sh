6192b3d979cbb90f3b3769ab36e3d4129513e2c2
Tom Hennen <TomHennen@users.noreply.github.com>
2024-12-20 11:04:16 -0500

    missed a SUMMARY_FILE

--- run.sh
+++ run.sh
@@ -37,7 +37,7 @@ do
     echo "```" >> $SUMMARY_FILE
     cat ./metadata/$tool/output.txt >> $SUMMARY_FILE
     echo "```" >> $SUMMARY_FILE
-    echo ""
+    echo "" >> $SUMMARY_FILE
 done
 echo "Done with all tools. Exiting with $WRANGLE_EXIT_STATUS"
 exit $WRANGLE_EXIT_STATUS
