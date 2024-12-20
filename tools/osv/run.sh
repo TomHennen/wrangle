3e08b110449af6a78d254b25e95b9e07971f2362
Tom Hennen <TomHennen@users.noreply.github.com>
2024-12-20 08:58:35 -0500

    tools will emit error if they fail

--- tools/osv/run.sh
+++ tools/osv/run.sh
@@ -2,9 +2,13 @@
 set -e
 
 echo "osv"
+
+WRANGLE_EXIT_STATUS=0
+
 # Run a scan over all of source and ignore errors, because OSV likes to fail
 # too often. :) (Maybe it generates an error code when it finds problems?)
-/osv-scanner --format sarif --output /metadata/osv.sarif -r /src || true
+/osv-scanner --format sarif --output /metadata/osv.sarif -r /src || (echo "osv failure when generating sarif") && WRANGLE_EXIT_STATUS=1
 # Run it again for the markdown. (if only we could output in multiple formats from one run...)
-/osv-scanner --format markdown --output /metadata/osv.md -r /src || true
+/osv-scanner --format markdown --output /metadata/osv.md -r /src || (echo "osv failure when generating markdown") && WRANGLE_EXIT_STATUS=1
 cat /metadata/osv.md
+exit $WRANGLE_EXIT_STATUS
--- tools/zizmor/run.sh
+++ tools/zizmor/run.sh
@@ -2,10 +2,12 @@
 set -e
 
 echo "zizmor"
+WRANGLE_EXIT_STATUS=0
 NO_COLOR=1
 echo "zizmor sarif"
-/usr/local/cargo/bin/zizmor --format sarif -o `find /src/.github/workflows -name "*.yml"` > /metadata/zizmor.sarif || true
+/usr/local/cargo/bin/zizmor --format sarif -o `find /src/.github/workflows -name "*.yml"` > /metadata/zizmor.sarif || (echo "zizmor failure when generating sarif") && WRANGLE_EXIT_STATUS=1
 # Run it again for the plain output. (if only we could output in multiple formats from one run...)
 echo "zizmor plain"
-/usr/local/cargo/bin/zizmor --format plain -o `find /src/.github/workflows -name "*.yml"` > /metadata/zizmor.txt || true
+/usr/local/cargo/bin/zizmor --format plain -o `find /src/.github/workflows -name "*.yml"` > /metadata/zizmor.txt || (echo "zizmor failure when generating text") && WRANGLE_EXIT_STATUS=1
 cat /metadata/zizmor.txt
+exit $WRANGLE_EXIT_STATUS
