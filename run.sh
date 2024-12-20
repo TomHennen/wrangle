80272c6254e119b8b388b42d429f06916fc963f6
Tom Hennen <TomHennen@users.noreply.github.com>
2024-12-20 10:40:48 -0500

    don't overwrite the file

--- run.sh
+++ run.sh
@@ -6,8 +6,8 @@ set -e
 
 mkdir ./metadata
 SUMMARY_FILE=./metadata/summary.md
-echo "| Tool | Status | Results |" > $SUMMARY_FILE
-echo "| ---- | ------ | ------- |" > $SUMMARY_FILE
+echo "| Tool | Status | Results |" >> $SUMMARY_FILE
+echo "| ---- | ------ | ------- |" >> $SUMMARY_FILE
 WRANGLE_EXIT_STATUS=0
 for tool in $@;
 do
@@ -23,7 +23,7 @@ do
        -v /var/run/docker.sock:/var/run/docker.sock \
 	   ghcr.io/tomhennen/wrangle/$tool:main | tee ./metadata/$tool/output.txt || WRANGLE_EXIT_STATUS=1; TOOL_STATUS="Failed"
     echo "$tool $TOOL_STATUS"
-    echo "| $tool | $TOOL_STATUS | `cat ./metadata/$tool/output.txt` |" > $SUMMARY_FILE
+    echo "| $tool | $TOOL_STATUS | <pre><code>`cat ./metadata/$tool/output.txt`</code></pre> |" >> $SUMMARY_FILE
 done
 echo "Done with all tools. Exiting with $WRANGLE_EXIT_STATUS"
 exit $WRANGLE_EXIT_STATUS
