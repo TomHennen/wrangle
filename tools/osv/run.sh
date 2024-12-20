6ca6a191bc5d75d17b82464fe8e609a39f5158bb
Tom Hennen <TomHennen@users.noreply.github.com>
2024-12-20 10:22:21 -0500

    fix how we set exit status

--- tools/osv/run.sh
+++ tools/osv/run.sh
@@ -7,8 +7,8 @@ WRANGLE_EXIT_STATUS=0
 
 # Run a scan over all of source and ignore errors, because OSV likes to fail
 # too often. :) (Maybe it generates an error code when it finds problems?)
-/osv-scanner --format sarif --output /metadata/osv.sarif -r /src || (echo "osv failure when generating sarif" && WRANGLE_EXIT_STATUS=1)
+/osv-scanner --format sarif --output /metadata/osv.sarif -r /src || echo "osv failure when generating sarif"; WRANGLE_EXIT_STATUS=1
 # Run it again for the markdown. (if only we could output in multiple formats from one run...)
-/osv-scanner --format markdown --output /metadata/osv.md -r /src || (echo "osv failure when generating markdown" && WRANGLE_EXIT_STATUS=1)
+/osv-scanner --format markdown --output /metadata/osv.md -r /src || echo "osv failure when generating markdown"; WRANGLE_EXIT_STATUS=1
 cat /metadata/osv.md
 exit $WRANGLE_EXIT_STATUS
