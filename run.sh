57c0fa13bc4ddb06bc836b458d1165f8c16eb60d
Tom Hennen <TomHennen@users.noreply.github.com>
2024-12-20 08:54:09 -0500

    Make it easier to tell when things fail

--- run.sh
+++ run.sh
@@ -4,16 +4,19 @@ set -e
 # Pass tools to run as arguments.
 # e.g. run.sh foo bar
 
+WRANGLE_EXIT_STATUS=0
 for tool in $@;
 do
     echo "Running $tool..."
     mkdir -p ./metadata/$tool
     mkdir -p ./dist/$tool
     docker run \
+       --quiet
        --mount type=bind,source=./dist/$tool,target=/dist \
 	   --mount type=bind,source=./metadata/$tool,target=/metadata \
 	   --mount type=bind,source=./,target=/src,readonly \
        -v /var/run/docker.sock:/var/run/docker.sock \
-	   ghcr.io/tomhennen/wrangle/$tool:main
+	   ghcr.io/tomhennen/wrangle/$tool:main || echo ("$tool failed") && WRANGLE_EXIT_STATUS=1
     echo "$tool done"
 done
+exit $WRANGLE_EXIT_STATUS
