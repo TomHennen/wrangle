9c2f16f985edbec8fa4ff2f8f12a2ea16e317eb7
Tom Hennen <TomHennen@users.noreply.github.com>
2024-12-20 11:19:27 -0500

    Get wrangle results displayed well (enough) (#1)
    Creates markdown with the tool results and displays that as a summary of the run.

--- run.sh
+++ run.sh
@@ -4,19 +4,38 @@ set -e
 # Pass tools to run as arguments.
 # e.g. run.sh foo bar
 
+mkdir ./metadata
+SUMMARY_FILE=./metadata/summary.md
+echo "# Wrangle results" >> $SUMMARY_FILE
+echo "| Tool | Status | Results |" >> $SUMMARY_FILE
+echo "| ---- | ------ | ------- |" >> $SUMMARY_FILE
 WRANGLE_EXIT_STATUS=0
 for tool in $@;
 do
     echo "Running $tool..."
     mkdir -p ./metadata/$tool
     mkdir -p ./dist/$tool
+    TOOL_STATUS="Success"
     docker run \
        --quiet \
        --mount type=bind,source=./dist/$tool,target=/dist \
 	   --mount type=bind,source=./metadata/$tool,target=/metadata \
 	   --mount type=bind,source=./,target=/src,readonly \
        -v /var/run/docker.sock:/var/run/docker.sock \
-	   ghcr.io/tomhennen/wrangle/$tool:main || (echo "$tool failed" && WRANGLE_EXIT_STATUS=1)
-    echo "$tool done"
+	   ghcr.io/tomhennen/wrangle/$tool:main | tee ./metadata/$tool/output.txt || WRANGLE_EXIT_STATUS=1; TOOL_STATUS="Failed"
+    echo "$tool $TOOL_STATUS"
+    echo "| $tool | $TOOL_STATUS | [Details](#$tool-details) |" >> $SUMMARY_FILE
 done
+
+echo "" >> $SUMMARY_FILE
+
+# Add in the details
+for tool in $@;
+do
+    echo "## $tool Details" >> $SUMMARY_FILE
+    printf "\n\n<pre><code>" >> $SUMMARY_FILE
+    cat ./metadata/$tool/output.txt >> $SUMMARY_FILE
+    printf "</code></pre>\n" >> $SUMMARY_FILE
+done
+echo "Done with all tools. Exiting with $WRANGLE_EXIT_STATUS"
 exit $WRANGLE_EXIT_STATUS
