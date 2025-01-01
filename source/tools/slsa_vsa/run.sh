5a6b74967c1a99b9eac434eae0966b7721a15669
Tom Hennen <tom.hennen@gmail.com>
2025-01-01 12:44:06 -0500

    cosign does too much with the attestation wrapper, need to figure that out...
    Signed-off-by: Tom Hennen <tom.hennen@gmail.com>

--- source/tools/slsa_vsa/run.sh
+++ source/tools/slsa_vsa/run.sh
@@ -9,4 +9,12 @@ set -e
 TIME_VERIFIED=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
 
 # Create the unsigned vsa
-jq -n --arg subjectCommit $1 --arg subjectRepo $2 --arg subjectBranch $3 --arg timeVerified $TIME_VERIFIED -f vsa_template.jq
+echo "Create unsigned source VSA"
+UNSIGNED_VSA=$(jq -n --arg subjectCommit $1 --arg subjectRepo $2 --arg subjectBranch $3 --arg timeVerified $TIME_VERIFIED -f vsa_template.jq)
+
+echo $UNSIGNED_VSA
+
+echo "Sign it"
+echo $UNSIGNED_VSA | cosign attest-blob --type https://slsa.dev/verification_summary/v1 --hash $1 --output-attestation vsa.att --yes --predicate - run.sh
+
+
