2fec0114de8c567e6c86afae2b66dd85074cdaa8
Tom Hennen <tom.hennen@gmail.com>
2025-01-01 17:31:00 -0500

    output bundle and document problems
    Signed-off-by: Tom Hennen <tom.hennen@gmail.com>

--- source/tools/slsa_vsa/run.sh
+++ source/tools/slsa_vsa/run.sh
@@ -15,6 +15,14 @@ UNSIGNED_VSA=$(jq -n --arg subjectCommit $1 --arg subjectRepo $2 --arg subjectBr
 echo $UNSIGNED_VSA
 
 echo "Sign it"
-echo $UNSIGNED_VSA | cosign attest-blob --type https://slsa.dev/verification_summary/v1 --hash $1 --output-attestation vsa.att --yes --predicate - run.sh
-
+# --output-attestation doesn't output the signed DSSE (but strangely the signed DSSE is sent to STDOUT).
+# So instead we write vsa.bundle which is a _Sigstore_ bundle and contains the DSSE.
+# run.sh is provided as the filename to compute the hash over even though we override the hash on the command line.
+# (otherwise we get errors).
+# Unfortunately this still doesn't work as is.  We're providing an in-toto _Statement_ not a predicate and we
+# _have to_ provide the entire statement since a SLSA Source VSA requires a gitCommit as the digest type and
+# we want to add annotations in the subject field. `attest-blob` doesn't let us do either of those things right
+# now.  So currently this embeds and entire statement inside a predicate field with the wrong subject.
+# We'll have to find some other way to sign things.
+echo $UNSIGNED_VSA | cosign attest-blob --type https://slsa.dev/verification_summary/v1 --hash "$1" --output-attestation vsa.att --bundle vsa.bundle --new-bundle-format=true --yes --predicate - run.sh
 
